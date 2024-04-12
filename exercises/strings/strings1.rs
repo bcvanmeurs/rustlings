d985982c7954ca5b98306d5ea8f9dfc25c0a8315
Bram van Meurs <7422223+bcvanmeurs@users.noreply.github.com>
2024-04-12 17:20:24 +0100

    strings

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -5,13 +5,15 @@
 // Execute `rustlings hint strings1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    "blue".to_string()
+    // this returns a string slice with 'static lifetime
+    // the data of the string lives in the code itself, so if will live as long as the program
+    // Create a String out of it
+    // String::from("blue") is an alternative
 }
--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -5,11 +5,13 @@
 // Execute `rustlings hint strings2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+
+    // If you're interested in learning about how this kind of
+    // reference conversion works, you can jump ahead in the book and read this part in the smart
+    // pointers chapter: https://doc.rust-lang.org/stable/book/ch15-02-deref.html#implicit-deref-coercions-with-functions-and-methods
+    if is_a_color_word(&word) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
