bb1b613ef3083ded60842125ac6765f47eaf1c6c
Bram van Meurs <7422223+bcvanmeurs@users.noreply.github.com>
2024-04-21 18:31:30 +0100

    quiz2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -31,12 +29,17 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            let string = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                // Command::Append(x) => string.to_string() + &"bar".repeat(*x),
+                Command::Append(x) => format!("{}{}", string.to_string(), "bar".repeat(*x)),
+                // https://stackoverflow.com/questions/75906310/rustlings-quiz2-why-is-i-of-type-usize-instead-of-usize
+            };
+            output.push(string);
         }
         output
     }
@@ -45,7 +48,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
