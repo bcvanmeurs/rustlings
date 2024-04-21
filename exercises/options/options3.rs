ee9c1bd93ff4aecd644fae10eacfc912364fb813
Bram van Meurs <7422223+bcvanmeurs@users.noreply.github.com>
2024-04-21 19:39:24 +0100

    options

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -14,8 +12,21 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        // https://doc.rust-lang.org/std/keyword.ref.html
+        // ref annotates pattern bindings to make them borrow rather than move.
+        // It is not a part of the pattern as far as matching is concerned: it does not affect
+        // whether a value is matched, only how it is matched.
+        // By default, match statements consume all they can, which can sometimes be a problem,
+        // when you don’t really need the value to be moved and owned.
+        // Using the ref keyword, the value is only borrowed, not moved, making it available for
+        // use after the match statement
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
+
+    // - & denotes that your pattern expects a reference to an object.
+    // Hence & is a part of said pattern: &Foo matches different objects than Foo does.
+    // ref indicates that you want a reference to an unpacked value.
+    // It is not matched against: Foo(ref foo) matches the same objects as Foo(foo)
 }
